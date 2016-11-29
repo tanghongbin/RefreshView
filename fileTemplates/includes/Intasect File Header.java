224dbe7c9c152db30c92e0615c79048d703ed429
Your Name <you@example.com>
2016-11-29 14:18:41 +0800

    AI-2.1.2 <dev-001@dev-001-PC Create editor.xml, other.xml, laf.xml, File Header.java, Class.java, web-browsers.xml, Intasect Class Header.java, Eclipse copy 0.xml, debugger.renderers.xml, jdk.table.xml, Interface.java, gradle.experimental.xml, Intasect File Header.java, Fragment.java, filetypes.xml, Activity.java, path.macros.xml, Default copy 0.xml, hg.xml, ide.general.xml, git.xml, ui.lnf.xml, project.default.xml, Default copy.xml, gitosc_settings.xml, androidStudioFirstRun.xml, keymap.xml, Default _1_.xml, debugger.xml, Enum.java, Eclipse copy.xml, colors.scheme.xml, notifications.xml, file.template.settings.xml, diff.xml, editor.codeinsight.xml, ignore.xml, gradle.run.settings.xml, Darcula copy.icls, toStringTemplates.xml, Default.xml, code.style.schemes.xml

---
+++ fileTemplates/includes/Intasect File Header.java
@@ -0,0 +1,12 @@
+/**
+*
+*@Title: ${NAME}
+* @Package ${PACKAGE_NAME}
+* @Description: TODO
+* Copyright: Copyright (c) 2016
+* Company: 成都壹柒互动科技有限公司
+* 
+* @author Android客户端组-ZhangHaiTao
+* @date ${DATE} ${TIME}
+* @version V1.0
+*/
